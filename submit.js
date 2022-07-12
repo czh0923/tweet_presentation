5c9a54da33c6583c507974812a034bb59cf7563e
czh0923 <zchenggn@126.com>
2022-07-11 19:03:13 -0700

    fixing bugs

--- submit.js
+++ submit.js
@@ -1,10 +1,11 @@
 
 async function submitData() {
 
-    url = "https://airtable-middle.herokuapp.com/put/" + localStorage.getItem(tweetUserNames) + "/" + 
-    localStorage.getItem(tweetUserIds) + "/" + JSON.stringify(participantInput) + "/" + participantId + 
-    localStorage.getItem(tweetUserPrevVisitedTimes) + "/" + localStorage.getItem(tweetUserPrevVisitedTimes);
+    url = "https://airtable-middle.herokuapp.com/put/" + localStorage.getItem("tweetUserNames") + "/" + 
+    localStorage.getItem("tweetUserIds") + "/" + JSON.stringify(participantInput) + "/" + participantID + "/" +
+    localStorage.getItem("tweetUserPrevVisitedTimes") + "/" + localStorage.getItem("tweetUserRecordIds");
 
+    // console.log(url);
     await fetch(url);
     console.log("submit and update complete");
     goToThanksPage();
